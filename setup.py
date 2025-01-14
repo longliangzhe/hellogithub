b65f6ca15eb0da8210000936b2c2204ca6fd5013
Lighting <17818883308@139.com>
2025-01-14 12:59:40 +0800

    11111

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hellogithub',
+    version='0.0.1',
+    packages=['hellogithub'],
+    url='https://github.com/longliangzhe/hellogithub',
+    license='GPL-3.0 License',
+    author='west5',
+    author_email='17818883308@139.com',
+    description=''
+)
